ae37fd3bdfb1b74ed7d9f5980aaa800e44dbf633
Aadhithya01 <aadhithyaraja180@gmail.com>
2026-05-27 08:31:27 +0530

    feat: add brand color palette to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          green: '#1a5c45',
+          gold: '#e8c96a',
+          cream: '#faf7f0',
+          border: '#e8e0d0',
+        },
+      },
+    },
+  },
   plugins: [],
 }
-
